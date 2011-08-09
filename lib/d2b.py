19535af67ca0b52caf4b03f9a051f5c816a8b1b7
Miguel Ángel García <miguelangel.garcia@gmail.com>
2011-08-09 17:49:56 +0200

    Parece que ya recupero los blogs

--- lib/d2b.py
+++ lib/d2b.py
@@ -22,20 +22,102 @@
 
 from optparse import OptionParser
 from optparse import OptionGroup
+import MySQLdb
 import sys
+import os
+
+
+class Blog(object):
+    def __init__(self):
+        self.title = ''
+        self.body = ''
+        self.nid = 0
+
+    def __str__(self):
+        return self.title + '\n\n' + self.body
 
 
 class D2B(object):
     def __init__(self, options):
         self.options = options
+        self.db = None
+        self.blogs = []
+
+    def __del__(self):
+        if self.db:
+            self.db.close()
+            self.db = None
 
     def run(self):
         if self.options.verbose:
             sys.stderr.write('Verbose mode is ON.\n')
-        if self.options.file:
-            sys.stderr.write('The file do not exists.\n')
-            return 2
+
+        self.__connect_to_database()
+        self.__load_file()
+        self.__get_blogs()
+        self.__write_to_hd()
+
         return 0
+
+    def __write_to_hd(self):
+        if not self.options.save_to_file:
+            return
+
+        for item in self.blogs:
+            fd = open("d2b_post_{0}.txt".format(item.nid), 'w+')
+            fd.write(str(item))
+            fd.close()
+
+    def __get_blogs(self):
+        if not self.db:
+            return
+
+        sql = "select nid,title,body from drupal_node_revisions order by nid"
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        for item in cursor:
+            blog = Blog()
+            blog.nid = item[0]
+            blog.title = item[1]
+            blog.body = item[2]
+            self.blogs.append(blog)
+        cursor.close()
+        self.db.commit()
+
+    def __load_file(self):
+        if not self.options.file:
+            return
+
+        self.__assert_file_exists()
+
+        self.__execute_script(self.__read_file())
+
+    def __execute_script(self, script):
+        cursor = self.db.cursor()
+        cursor.execute(script)
+        cursor.close()
+        self.db.commit()
+
+    def __read_file(self):
+        fd = open(self.options.file)
+        result = fd.read()
+        fd.close()
+        return result
+
+    def __assert_file_exists(self):
+        if not os.path.exists(self.options.file):
+            sys.stderr.write('The file do not exists.\n')
+            sys.exit(2)
+
+    def __connect_to_database(self):
+        if not self.options.user or \
+                not self.options.password or \
+                not self.options.database:
+            return
+
+        self.db = MySQLdb.connect(user=self.options.user,
+                                  passwd=self.options.password,
+                                  db=self.options.database)
 
 
 def main():
@@ -60,6 +142,9 @@ def main():
                     help='Password to connecto to DDBB.')
     group.add_option('-d', '--database', action='store',
                     dest='database',
+                    help='Database to connect to.')
+    group.add_option('--save-to-file', action='store_true', default=False,
+                    dest='save_to_file',
                     help='Database to connect to.')
 
     parser.add_option_group(group)
--- tests/database/example001.sql
+++ tests/database/example001.sql
@@ -1,24 +1,18 @@
-CREATE TABLE IF NOT EXISTS `drupal_comments` (
-  `cid` int(11) NOT NULL auto_increment,
-  `pid` int(11) NOT NULL default '0',
-  `nid` int(11) NOT NULL default '0',
+CREATE TABLE IF NOT EXISTS `drupal_node_revisions` (
+  `nid` int(10) unsigned NOT NULL default '0',
+  `vid` int(10) unsigned NOT NULL auto_increment,
   `uid` int(11) NOT NULL default '0',
-  `subject` varchar(64) NOT NULL default '',
-  `comment` longtext NOT NULL,
-  `hostname` varchar(128) NOT NULL default '',
+  `title` varchar(255) NOT NULL default '',
+  `body` longtext NOT NULL,
+  `teaser` longtext NOT NULL,
+  `log` longtext NOT NULL,
   `timestamp` int(11) NOT NULL default '0',
-  `status` tinyint(3) unsigned NOT NULL default '0',
-  `format` smallint(6) NOT NULL default '0',
-  `thread` varchar(255) NOT NULL,
-  `name` varchar(60) default NULL,
-  `mail` varchar(64) default NULL,
-  `homepage` varchar(255) default NULL,
-  PRIMARY KEY  (`cid`),
-  KEY `pid` (`pid`),
+  `format` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`vid`),
   KEY `nid` (`nid`),
-  KEY `status` (`status`)
-) ENGINE=MyISAM AUTO_INCREMENT=2367 DEFAULT CHARSET=utf8 AUTO_INCREMENT=2367 ;
+  KEY `uid` (`uid`)
+) ENGINE=MyISAM AUTO_INCREMENT=183 DEFAULT CHARSET=utf8 AUTO_INCREMENT=183 ;
 
 
-INSERT INTO `drupal_comments` VALUES (1, 0, 38, 0, 'Ejemplo 1 de comentario', 'Esto es un ejemplo de comentario.', '127.0.0.1', 1262969466, 0, 2, '01/', '', '', '');
-INSERT INTO `drupal_comments` VALUES (2, 0, 38, 1, 'Ejemplo 2 de comentario', 'Esto es otro ejemplo de comentario', '127.0.0.1', 1263028149, 0, 2, '02/', '', '', '');
+INSERT INTO `drupal_node_revisions` VALUES (1, 1, 1, 'Bienvenida', 'Probando...', 'Probando...', '', 1261126931, 2);
+INSERT INTO `drupal_node_revisions` VALUES (2, 2, 1, 'Bienvenida2', 'Probando2...', 'Probando2...', '', 1261126931, 2);
